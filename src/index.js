a77217c90d96e06738b2e4057f63f78600f71f6c
Ahmet Emin ŞİT <sitahmetemin@hotmail.com>
2019-10-24 15:38:21 +0300

    bootstrap eklendi ve menü düzenlendi

--- src/index.js
+++ src/index.js
@@ -1,57 +1,72 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+if (require("electron-squirrel-startup")) {
+    app.quit();
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        icon: path.join(__dirname, "assets/img/icon/main.png")
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+const mainMenuTemplate = [
+    {
+        label: "Dosya",
+        submenu: [
+            {
+                label: "Çıkış",
+                accelerator: "Ctrl+Q",
+                role: "quit"
+            }
+        ]
+    }
+];
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+if (process.env.NODE_ENV !== "production") {
+    mainMenuTemplate.push(
+        {
+            label: "DevTools",
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+            },
+            accelerator:"Ctrl+T"
+        },
+        {
+            label: "Yenile",
+            role: "Reload",
+            accelerator: "Ctrl+R"
+        }
+    );
+}
